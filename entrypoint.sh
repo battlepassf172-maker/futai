b65ca4572556ff956d20c23446234b0f686d2ea1
battlepassf172-maker <battlepassf172@gmail.com>
2026-05-03 20:29:13 +0300

    Add entrypoint script for SSH and VNC server setup

---
+++ entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+/usr/sbin/sshd
+
+vncserver -localhost no -SecurityTypes None -geometry 1024x768 --I-KNOW-THIS-IS-INSECURE
+
+openssl req -new -subj "/C=JP" -x509 -days 365 -nodes -out /self.pem -keyout /self.pem
+
+websockify -D --web=/usr/share/novnc/ --cert=/self.pem 6080 localhost:5901
+
+tail -f /dev/null
